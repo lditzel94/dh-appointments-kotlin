ee5836582ae1ffc6e3a5a12be3c3bafb17235e53
lditzel94 <lucianoditzel94@gmail.com>
2023-02-17 14:57:15 -0300

    add application files

--- src/main/kotlin/com/digitalhouse/appointments/AppointmentsApplication.kt
+++ src/main/kotlin/com/digitalhouse/appointments/AppointmentsApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AppointmentsKotlinApplication
+class AppointmentsApplication
 
 fun main(args: Array<String>) {
-    runApplication<AppointmentsKotlinApplication>(*args)
+    runApplication<AppointmentsApplication>(*args)
 }
